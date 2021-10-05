5784feb3f791e27e39633c27781e91bbb74a758d
Mitchell <d.r.mitchellriley@gmail.com>
2021-10-04 22:24:52 -0700

    Geoscript api and local storage works with form

---
+++ script.js
@@ -0,0 +1,93 @@
+var userinput = document.getElementById('input')
+var prevcities = document.getElementsByClassName('prev-cities')
+var searchbtn = document.getElementById('search-button');
+var usercity = document.getElementById('city-search')
+var citylist = [];
+
+//https://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+function initialforecast() {
+    //var citysearch1 = citysearch.replace(/\s+/g, "");
+    
+    // if(!citysearch) {
+        // alert('Please put in a city name!');
+        // return;
+        // }
+        console.log(searchlink)
+    }
+    
+    function storecities() {
+        localStorage.setItem('cities', JSON.stringify(citylist));
+    }
+    
+    
+    function fetchweather() {
+        var citysearch = document.getElementById("city-search").value;
+        var citysearch2 = citysearch.replace(" ", '+')
+        var searchlink = 'https://api.openweathermap.org/geo/1.0/direct?q=' + citysearch2 + '&limit=1&appid=bb789583263105d6bc95c93685382fbc';
+        console.log(searchlink)
+        fetch(searchlink)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data){
+            console.log(data)
+        })
+    }
+    
+    
+    function addcity() {
+        prevcities.innerHTML = "";
+        
+        for (var i = 0; i < citylist.length; i++) {
+            var city = citylist[i];
+            var li = document.createElement('li');
+            button.textContent = city;
+            button.setAttribute('data-index', i);
+            
+            
+        }
+    }
+    
+    userinput.addEventListener('submit' , function(event) {
+        event.preventDefault();
+        fetchweather();
+        var cityname = usercity.value.trim();
+        console.log(cityname)
+        if (cityname === '') {
+            return;
+        }
+        citylist.push(cityname);
+        usercity.value = ''
+        storecities();
+    });
+    
+    
+    // prevcities.addEventListener('click', function(event) {
+        //     var element = event.target;
+        
+        //     if (element.matches('button') === true) {
+            //         var 
+            //     }
+            // })
+            
+            
+            
+            //searchbtn.addEventListener('click', initialforecast);
+            
+            // var getRepoIssues = function (repo) {
+                
+                //   fetch(searchlink)
+                //     .then(function (response) {
+                    //       if (response.ok) {
+                        //         // What is this checking for? Under what condition will this be `true`?
+//         // TODO: Write your answer here
+//         if (response.headers.get('Link')) {
+//           displayWarning(repo);
+//         }
+//         return response.json();
+//       }
+//       document.getElementById('li1').textContent = searchlink
+//     })
+//     .then(displayIssues);
+// };
